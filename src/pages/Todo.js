e062370fec58de90b360e3498da62c5f1aa7f159
danial-nas17 <149297045+danial-nas17@users.noreply.github.com>
2023-11-23 23:09:59 +0330

    create todo

--- src/pages/Todo.js
+++ src/pages/Todo.js
@@ -1,12 +1,14 @@
 import React, { useContext, useEffect, useState } from 'react'
 import { TodoContext } from '../component/context/TodoContext'
-import axios from 'axios';
+import "./todo.css"
+import FilterTodos from '../component/todos/FilterTodos';
+import CreateTodos from '../component/todos/CreateTodos';
+
+
 
 function Todo() {
     const [loading , setLoading] = useState(true)
-    const [error , setError] = useState(null)
-
-    const {todos , getTodos} = useContext(TodoContext)
+    const {todos , getTodos , error} = useContext(TodoContext)
 
     useEffect( () =>{
 
@@ -22,20 +24,34 @@ function Todo() {
 
   return (
     <>
+        <div className="container mt-4">
+            <div className="row g-3 ">
+                <CreateTodos/>
+                <hr/>
+                <FilterTodos/>
+                {loading && <div className="spinner-border  text-center text-primary mt-5 ms-4" role="status">
+                    <span className="visually-hidden">Loading...</span>
+                    </div>}
 
-        {loading && <div className="spinner-border text-danger text-center mt-2 ms-4" role="status">
-            <span className="visually-hidden">Loading...</span>
-            </div>}
-
-        {error && <div className='text-center text-danger'>error is happend</div>}
-    
-        {todos && todos.map(todo => (
-            <h1 key={todo.id}>{todo.title}</h1>
-        ))}
+                {error && <div className='text-center text-danger fs-5 mt-5'>{error}</div>}
+            
+                {todos && todos.map(todo => (
+                    <div key={todo.id} className="col-md-8 d-flex justify-content-between ali align-items-center  dani">
+                       <div className='dani2'>{todo.completed ? <del>{todo.title}</del> : <span>{todo.title}</span>}</div> 
+                       <div className="d-flex align-items-center p-2">
+                            {todo.completed ?
+                            <i className="bi bi-check-all"></i> 
+                            : 
+                            <i className="bi bi-check"></i>
+                             }
+                             <i className="bi bi-trash-fill"></i>
+                       </div>
+                    </div>
+                ))}
 
    
-   
-   
+        </div>
+        </div>  
    
     
     </>
